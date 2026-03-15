b87f88f0c10451e063cd71a2d0ebe4abb545a661
Ritikraj2003 <ritikraj1092002@gmail.com>
2026-03-16 01:46:17 +0530

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { AboutComponent } from './modules/pages/about/about.component';
+import { BlogComponent } from './modules/pages/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'OM Structures – Home',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'OM Structures – About Us',
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+    title: 'OM Structures – Blog',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
